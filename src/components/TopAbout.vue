4f2b0eab08952fd99196b088ad96bd2e9fb8642a
clf94-dev <clf94@hotmail.es>
2020-11-05 07:56:39 +0100

    added TopAbout component struct and style

--- src/components/TopAbout.vue
+++ src/components/TopAbout.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-    <h1>Top about component</h1>
+<div class='top-about-cont'>
+    <h1>We empower innovators by delivering access to the financial system</h1>
 </div>
 </template>
 
@@ -11,5 +11,33 @@ export default {
 </script>
 
 <style scoped>
+.top-about-cont {
+    padding: 5% 7%;
+}
 
+@media screen and (min-width: 1080px) {
+    .top-about-cont {
+        padding: 5% 17%;
+    }
+}
+
+.top-about-cont h1 {
+    font-size: 56px;
+    font-family: "Public Sans", sans-serif;
+    font-weight: 300;
+    color: #1b262f;
+}
+
+@media screen and (min-width: 1080px) {
+    .top-about-cont h1 {
+        padding-right: 25%;
+    }
+}
+
+@media screen and (max-width: 1080px) {
+    .top-about-cont h1 {
+        text-align: center;
+        font-size: 35px;
+    }
+}
 </style>
