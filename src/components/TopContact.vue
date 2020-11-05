39d52a61a8295e42fc87b0231a06b31f75d3bc5e
clf94-dev <clf94@hotmail.es>
2020-11-05 07:55:09 +0100

    added TopContact struct and style

--- src/components/TopContact.vue
+++ src/components/TopContact.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-    <h1>Top contact component</h1>
+<div class='top-contact-cont'>
+    <h1>Submit a help request and we’ll get in touch shortly.</h1>
 </div>
 </template>
 
@@ -11,5 +11,33 @@ export default {
 </script>
 
 <style scoped>
+.top-contact-cont {
+    padding: 5% 7% 0%;
+}
 
+@media screen and (min-width: 1080px) {
+    .top-contact-cont {
+        padding: 5% 10% 0%;
+    }
+}
+
+.top-contact-cont h1 {
+    font-size: 56px;
+    font-family: "Public Sans", sans-serif;
+    font-weight: 300;
+    color: #1b262f;
+}
+
+@media screen and (min-width: 800px) {
+    .top-contact-cont h1 {
+        padding-right: 35% !important;
+    }
+}
+
+@media screen and (max-width: 800px) {
+    .top-contact-cont h1 {
+        text-align: center;
+        font-size: 35px;
+    }
+}
 </style>
