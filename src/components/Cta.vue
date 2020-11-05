a1026253236550be0c9fb204161d0a28672b12da
clf94-dev <clf94@hotmail.es>
2020-11-05 09:20:18 +0100

    added cta component struct

--- src/components/Cta.vue
+++ src/components/Cta.vue
@@ -1,8 +1,25 @@
 <template>
-<div>
-    <h1>Cta component</h1>
+<b-container fluid>
+    <b-row class="cta-section">
+        <b-col class="title-col" xs="12" sm="12" md="6" lg="6">
+            <h2>Ready to start?</h2>
+        </b-col>
+        <b-col class="input-row" xs="12" sm="12" md="6" lg="6">
+            <form>
+                <b-row>
+                    <b-col xs="12" sm="8" md="8" lg="8" class="input-col">
+                        <input type="text">
+                    </b-col>
+                    <b-col xs="12" sm="4" md="4" lg="4" class="btn-col">
+                        <button type="submit" class='pink-btn'>Schedule a Demo</button>
+                    </b-col>
+                </b-row>
 
-</div>
+            </form>
+        </b-col>
+
+    </b-row>
+</b-container>
 </template>
 
 <script>
