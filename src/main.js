5245d25fb1bf66f188e71389a91ecbb3b10fec6d
clf94-dev <clf94@hotmail.es>
2020-11-05 07:35:58 +0100

    added routes config

--- src/main.js
+++ src/main.js
@@ -3,9 +3,32 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import '@fortawesome/fontawesome-free/css/all.css'     
+import '@fortawesome/fontawesome-free/js/all.js'  
+
+import Home from './components/pages/Home.vue'
+import Pricing from './components/pages/Pricing.vue'
+import About from './components/pages/About.vue'
+import Contact from './components/pages/Contact.vue'
+import VueRouter from 'vue-router'
+
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes=[
+  {path:'/', component: Home},
+  {path:'/home', component: Home},
+  {path:'/pricing', component: Pricing},
+  {path:'/about', component: About },
+  {path:'/contact', component: Contact }
+];
+
+const router=new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 new Vue({
-  render: h => h(App),
+  router,render: h => h(App),
 }).$mount('#app')
