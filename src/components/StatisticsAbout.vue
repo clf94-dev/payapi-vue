3a168b1849d709527d34eb4d9b2eea15e652dab3
clf94-dev <clf94@hotmail.es>
2020-11-07 07:40:30 +0100

    added struct statistics component

--- src/components/StatisticsAbout.vue
+++ src/components/StatisticsAbout.vue
@@ -1,12 +1,41 @@
 <template>
-<div>
-    <h1>Statistics about component</h1>
-</div>
+<b-container fluid class="statistics-section">
+    <img :src="study" alt="Team members">
+    <b-row class="statistics-row">
+        <hr class="block-div" />
+        <b-col class="statistics-col" v-for="data of statistics" v-bind:key="data.title">
+
+            <hr class="card-hr" />
+            <h3>{{data.title}}</h3>
+            <h1>{{data.text}}</h1>
+            <hr class="card-hr" />
+        </b-col>
+        <hr class="block-div">
+    </b-row>
+</b-container>
 </template>
 
 <script>
 export default {
-    name: 'StatisticsAbout'
+    name: 'StatisticsAbout',
+    data() {
+        return {
+            study: require("@/assets/images/about/desktop/image-team-members.jpg"),
+            statistics: [{
+                    title: 'Team Members',
+                    text: '300+'
+                },
+                {
+                    title: 'Offices in the US',
+                    text: '3'
+                },
+                {
+                    title: 'Transactions analyzed',
+                    text: '10M+'
+                }
+            ]
+        }
+    }
 }
 </script>
 
