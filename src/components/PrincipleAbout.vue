1d19e74429d2c015309e7618e60b64df488a9763
clf94-dev <clf94@hotmail.es>
2020-11-09 09:18:58 +0100

    added data and principle component (props)

--- src/components/PrincipleAbout.vue
+++ src/components/PrincipleAbout.vue
@@ -1,12 +1,21 @@
 <template>
-<div>
-    <h1>Principle about component </h1>
-</div>
+<b-container fluid class="principles-section">
+    <b-row v-for="data of info" v-bind:key="data.title">
+        <b-col xs="12" sm="12" md="6" lg="6" class="title-col">
+            <h4>{{data.title}}</h4>
+        </b-col>
+        <b-col xs="12" sm="12" md="6" lg="6" class="text-col">
+            <p>{{data.text}}</p>
+        </b-col>
+    </b-row>
+</b-container>
 </template>
 
 <script>
 export default {
-    name: 'PrincipleAbout'
+    name: 'PrincipleAbout',
+    props: ['info']
+
 }
 </script>
 
--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,9 +1,9 @@
 <template>
 <div>
     <TopAbout></TopAbout>
-    <PrincipleAbout></PrincipleAbout>
+    <PrincipleAbout :info="principles1"></PrincipleAbout>
     <StatisticsAbout></StatisticsAbout>
-    <PrincipleAbout></PrincipleAbout>
+    <PrincipleAbout :info="principles2"></PrincipleAbout>
 </div>
 </template>
 
@@ -18,6 +18,24 @@ export default {
         PrincipleAbout,
         StatisticsAbout,
         TopAbout
+    },
+    data() {
+        return {
+            principles1: [{
+                title: 'Our Vision',
+                text: 'Our main goal is to build beautiful consumer experiences along with developer-friendly infrastructure. The result is an intelligent tool that gives everyone the ability to create amazing products that solve big problems. We are deeply focused on democratizing financial services through technology.'
+            }, {
+                title: 'Our Business',
+                text: 'At the core of our platform is the technical infrastructure APIs that connect consumers. Our innovative product provides key insights for businesses and individuals, as well as robust reporting for traditional financial institutions and developers.'
+            }],
+            principles2: [{
+                title: 'The Culture',
+                text: "We strongly believe there's always an opportunity to learn from each other outside of day-to-day work, whether it's company-wide offsites, internal hackathons, or co-worker meetups. We always value cross-team collaboration and diversity of thought, no matter the job title."
+            }, {
+                title: "The People",
+                text: "We're all passionate about building a more efficient and inclusive financial infrastructure together. At PayAPI, we have diverse backgrounds and skills."
+            }],
+        }
     }
 }
 </script>
