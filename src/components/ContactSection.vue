fea68e676e178bfa9d150edf9672ef1f1ae806d8
clf94-dev <clf94@hotmail.es>
2020-11-10 10:51:12 +0100

    added struct contact form component

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,7 +1,23 @@
 <template>
-<div>
-    <h1>Contact section component</h1>
-</div>
+<b-container fluid class="contact-section">
+
+    <b-row>
+        <b-col xs="12" sm="12" md="6" lg="6" class="form">
+            <form class="form-col">
+                <input type="text" name="Name" placeholder="Name">\
+                <input type="email" name="email" id="" placeholder="Email Address">
+                <input type="text" placeholder="Company-name">
+                <input type="text" placeholder="Title">
+                <textarea name="message" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                <input type="checkbox" name="checkbox" id="" class="checkbox" />
+                <label for="checkbox">Stay up-to-date with company announcements and updates to our
+                    API</label>
+                <button type="submit" class="submit-btn"> Submit </button>
+            </form>
+        </b-col>
+
+    </b-row>
+</b-container>
 </template>
 
 <script>
