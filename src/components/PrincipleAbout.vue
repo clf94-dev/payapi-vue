e5b153a18372e42788d77669ce2df9babdef311c
clf94-dev <clf94@hotmail.es>
2020-11-17 09:33:34 +0100

    added AOS pricinples section

--- src/components/PrincipleAbout.vue
+++ src/components/PrincipleAbout.vue
@@ -2,10 +2,10 @@
 <b-container fluid class="principles-section">
     <b-row v-for="data of info" v-bind:key="data.title">
         <b-col xs="12" sm="12" md="6" lg="6" class="title-col">
-            <h4>{{data.title}}</h4>
+            <h4 data-aos='fade-right' :data-aos-delay="data.delayT"> {{data.title}}</h4>
         </b-col>
         <b-col xs="12" sm="12" md="6" lg="6" class="text-col">
-            <p>{{data.text}}</p>
+            <p data-aos='fade-left' :data-aos-delay="data.delayt">{{data.text}}</p>
         </b-col>
     </b-row>
 </b-container>
--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -23,17 +23,25 @@ export default {
         return {
             principles1: [{
                 title: 'Our Vision',
-                text: 'Our main goal is to build beautiful consumer experiences along with developer-friendly infrastructure. The result is an intelligent tool that gives everyone the ability to create amazing products that solve big problems. We are deeply focused on democratizing financial services through technology.'
+                text: 'Our main goal is to build beautiful consumer experiences along with developer-friendly infrastructure. The result is an intelligent tool that gives everyone the ability to create amazing products that solve big problems. We are deeply focused on democratizing financial services through technology.',
+                delayT: 200,
+                delayt: 300
             }, {
                 title: 'Our Business',
-                text: 'At the core of our platform is the technical infrastructure APIs that connect consumers. Our innovative product provides key insights for businesses and individuals, as well as robust reporting for traditional financial institutions and developers.'
+                text: 'At the core of our platform is the technical infrastructure APIs that connect consumers. Our innovative product provides key insights for businesses and individuals, as well as robust reporting for traditional financial institutions and developers.',
+                delayT: 400,
+                delayt: 500
             }],
             principles2: [{
                 title: 'The Culture',
-                text: "We strongly believe there's always an opportunity to learn from each other outside of day-to-day work, whether it's company-wide offsites, internal hackathons, or co-worker meetups. We always value cross-team collaboration and diversity of thought, no matter the job title."
+                text: "We strongly believe there's always an opportunity to learn from each other outside of day-to-day work, whether it's company-wide offsites, internal hackathons, or co-worker meetups. We always value cross-team collaboration and diversity of thought, no matter the job title.",
+                delayT: 200,
+                delayt: 300
             }, {
                 title: "The People",
-                text: "We're all passionate about building a more efficient and inclusive financial infrastructure together. At PayAPI, we have diverse backgrounds and skills."
+                text: "We're all passionate about building a more efficient and inclusive financial infrastructure together. At PayAPI, we have diverse backgrounds and skills.",
+                delayT: 400,
+                delayt: 500
             }],
         }
     }
